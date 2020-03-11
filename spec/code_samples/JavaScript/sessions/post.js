58b5bb4dac3963a6b2bb87d36540322f8192499c
Ruby <rdejesus@ualberta.ca>
2020-03-11 06:59:28 -0600

    generalized api info

---
+++ spec/code_samples/JavaScript/sessions/post.js
@@ -0,0 +1,8 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var url = "http://127.0.0.1:8000/vibrantminds2/api/sessions/"
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("POST", url);
+xmlhttp.send();
+xmlhttp.onreadystatechange = (e) => {
+    console.log(xmlhttp.responseText)
+}
